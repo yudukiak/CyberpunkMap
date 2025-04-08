db5d0a0958b84d477d22d22e80408829801bd2bd
紅坂柚月 <akasaka@ydk.vc>
2025-04-08 23:05:21 +0900

    [add] shadcn/ui コンポーネントを追加

--- Server/vite.config.ts
+++ Server/vite.config.ts
@@ -2,9 +2,15 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from 'path'
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'app'),
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
