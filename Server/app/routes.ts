6d1fcccbfef93264102cbeeee616d7d1d61d806d
紅坂柚月 <akasaka@ydk.vc>
2025-04-08 10:13:35 +0900

    [delete] apiを削除

--- Server/app/routes.ts
+++ Server/app/routes.ts
@@ -3,5 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/index.tsx"),
   route("red", "routes/red.tsx"),
-  route("api", "routes/api.tsx"),
 ] satisfies RouteConfig;
