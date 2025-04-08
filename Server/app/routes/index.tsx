709b696ef480cad6beaf45e497b93a2fcc3d2e9a
紅坂柚月 <akasaka@ydk.vc>
2025-04-08 11:14:24 +0900

    [fix] リダイレクトを変更

--- Server/app/routes/index.tsx
+++ Server/app/routes/index.tsx
@@ -2,7 +2,7 @@ import type { Route } from "./+types/index";
 import { redirect } from "react-router";
 
 export async function loader() {
-  throw redirect("/red");
+  return redirect("/red");
 }
 
 export default function Index() {
