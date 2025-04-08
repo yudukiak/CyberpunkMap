f5e754f3e639581a7f70a58f7bf3b4557732d572
紅坂柚月 <akasaka@ydk.vc>
2025-04-08 11:14:45 +0900

    [fix] アイコンをルートに変更

--- Server/app/root.tsx
+++ Server/app/root.tsx
@@ -22,12 +22,12 @@ export const links: Route.LinksFunction = () => [
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
   // Favicon & Icons
-  { rel: "icon", type: "image/png", sizes: "96x96", href: "/icon/favicon-96x96.png" },
-  { rel: "icon", type: "image/svg+xml", href: "/icon/favicon.svg" },
-  { rel: "shortcut icon", href: "/icon/favicon.ico" },
-  { rel: "apple-touch-icon", href: "/icon/favicon.ico" },
-  { rel: "apple-touch-icon", sizes: "180x180", href: "/icon/apple-touch-icon.png" },
-  { rel: "manifest", href: "/icon/site.webmanifest" },
+  { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png" },
+  { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+  { rel: "shortcut icon", href: "/favicon.ico" },
+  { rel: "apple-touch-icon", href: "/favicon.ico" },
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  { rel: "manifest", href: "/site.webmanifest" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
