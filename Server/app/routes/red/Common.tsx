6cf51a53a5e22d7ac202486025409ea813ff6e38
紅坂柚月 <akasaka@ydk.vc>
2025-04-11 01:25:05 +0900

    [update] マップの読み込みを変更

--- Server/app/routes/red/Common.tsx
+++ Server/app/routes/red/Common.tsx
@@ -1,17 +1,15 @@
 import type { PinsObjectType } from "types/map";
-import { useState, useEffect } from "react";
+import { lazy, Suspense } from "react";
 import Loading from "~/views/Loading";
 
 export default function Common({ pins }: { pins: PinsObjectType[] }) {
   // クライアントでのみ Leaflet を読み込む
-  const [Map, setMap] = useState<React.FC<any> | null>(null);
-  useEffect(() => {
-    import("./Map").then((mod) => {
-      setMap(() => mod.default);
-    });
-  }, []);
-
-  // マップを描写する？
-  const isReady = pins && Map;
-  return <main>{isReady ? <Map pins={pins} /> : <Loading />}</main>;
+  const Map = typeof window !== 'undefined' ? lazy(() => import('./Map')) : () => null;
+  return (
+    <main>
+      <Suspense fallback={<Loading />}>
+        <Map pins={pins} />
+      </Suspense>
+    </main>
+  );
 }
