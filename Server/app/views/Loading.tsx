54670a66fa2c43e0c96afcbfe978cca1c32dada2
紅坂柚月 <akasaka@ydk.vc>
2025-04-14 00:46:02 +0900

    [fix] ロードのClassNameを調整

--- Server/app/views/Loading.tsx
+++ Server/app/views/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="h-full w-full flex items-center justify-center">
       <div className="animate-spin rounded-full h-16 w-16 border-8 border-red-600 border-t-gray-600" />
     </div>
   );
