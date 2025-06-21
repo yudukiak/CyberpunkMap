256e28efe7ff62dfc3644078903c359f6a3114a2
紅坂柚月 <akasaka@ydk.vc>
2025-06-21 23:08:40 +0900

    [fix] title

--- Server/app/routes/edge/index.tsx
+++ Server/app/routes/edge/index.tsx
@@ -1,6 +1,10 @@
 import Error from "~/components/error";
 import Map from "~/features/map-view";
 
+export function meta() {
+  return [{ title: "Cyberpunk Edgerunners Map" }];
+}
+
 export default function Index() {
   return (
     <main className="h-dvh w-dvw">
