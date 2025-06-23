101f60f0fc7d34df12e11a4be9a43f44b8ad2315
紅坂柚月 <akasaka@ydk.vc>
2025-06-23 22:38:38 +0900

    [fix] 本番では利用しないためDevを付与

--- Server/app/utils/websocket-url.ts
+++ Server/app/utils/websocket-url.ts
@@ -1,10 +1,10 @@
-import { isDev, DEV_WS_PORT, SERVER_PORT } from "~/config/client";
+import { isDev, DEV_WS_PORT, DEV_SV_PORT } from "~/config/client";
 
 export function getWebsocketUrl() {
   const isHttp = window.location.protocol === "http:";
   const wsUrl = 
     isDev ? `ws://${window.location.hostname}:${DEV_WS_PORT}/ws` : 
-    isHttp ? `ws://${window.location.hostname}:${SERVER_PORT}/ws` :
+    isHttp ? `ws://${window.location.hostname}:${DEV_SV_PORT}/ws` :
     `wss://${window.location.hostname}/ws`;
   return wsUrl;
 }
